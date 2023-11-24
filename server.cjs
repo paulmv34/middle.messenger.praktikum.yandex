0ab50c1358bd80cfb961a7c084a4638f92dd2706
Pavel Vinogradov <disintegration777@yandex.ru>
2023-11-24 04:20:35 +0300

    Убран лишний комментарий

--- server.cjs
+++ server.cjs
@@ -6,18 +6,6 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
-/*
-['/', '/sign-up', '/settings', '/messenger'].forEach(route => {
-  app.get(route, function (req, res) {
-    res.status(200).sendFile(path.join(__dirname, './dist/index.html'));
-  })
-});
-
-app.get('*', function (req, res) {
-  res.redirect('/');
-})
-*/
-
 app.get('*', function (req, res) {
     res.status(200).sendFile(path.join(__dirname, './dist/index.html'));
 });
